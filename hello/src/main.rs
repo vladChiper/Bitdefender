e71bf9bf82d29f20811fbe39be08817d29391fb2
vladChiper <vladcartofipai@gmail.com>
2026-04-20 16:24:34 +0300

    no warnings

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,17 +1,7 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::path::Path;
 use zip::ZipArchive;
 
-fn display_filenames(paths: Vec<&str>) {
-    for path in paths {
-        if let Some(filename) = Path::new(path).file_name() {
-            if let Some(name) = filename.to_str() {
-                println!("{}", name);
-            }
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. Open the file
@@ -25,15 +15,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 0..archive.len() {
         let file = archive.by_index(i)?;
         let filename = file.name();
-        let (a, b) = filename.rsplit_once('/').unwrap();
-        if(b == "") {
+        let (_a, b) = filename.rsplit_once('/').unwrap();
+        if b == "" {
             continue;
         }
         println!("Filename: {}", b);
         
-        // Example: Reading content of a file
-        // use std::io::Read;
-        // file.read_to_end(&mut buffer)?;
     }
     Ok(())
 }
